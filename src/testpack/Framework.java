830324f86f26704552132a3ff3062d7b79737e8d
teamlead11 <automationlead11@gmail.com>
2018-07-23 09:40:26 +0530

    framework changes

---
+++ src/testpack/Framework.java
@@ -0,0 +1,14 @@
+package testpack;
+
+public class Framework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		myframeworkComponents();
+	}
+
+	public static void myframeworkComponents() {
+		System.out.println("this is my testing framework");
+	}
+}
