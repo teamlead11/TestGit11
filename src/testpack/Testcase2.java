8b2f7dfe62b7768d3542a416f991518bd4710002
teamlead11 <automationlead11@gmail.com>
2018-07-25 08:51:24 +0530

    added add logic for test case2

--- src/testpack/Testcase2.java
+++ src/testpack/Testcase2.java
@@ -4,11 +4,14 @@ public class Testcase2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		testcase2();
+		testcase2(5, 4);
 	}
 
-	public static void testcase2() {
-		System.out.println("this is my test case1");
+	public static void testcase2(int a, int b) {
+		System.out.println("this is my test case2");
+		int c = a + b;
+		System.out.println(c);
+
 	}
 
 }
